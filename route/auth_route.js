be5dc716e4ae06dc988c6bd3772ca37e74851735
hakraoul <haknjr11@gmail.com>
2024-08-21 22:36:17 +0700

    close #1 add login function

--- route/auth_route.js
+++ route/auth_route.js
@@ -1,7 +1,9 @@
-const { signUp } = require("../controller/auth_controller");
+const { signUp, login } = require("../controller/auth_controller");
 
 const router = require("express").Router();
 
 router.route("/sign-up").post(signUp);
+
+router.route("/login").post(login);
 
 module.exports = router;
